3a9dc40536ddf67d13e560e3cbe3f3ce15117fed
brandononeal <baoneal95@gmail.com>
2020-10-01 23:54:29 -0700

    mvp complete

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import { useLocalStorage } from "./useLocalStorage";
+
+const useDarkMode = (key, initialValue) => {
+  const [values, setValues] = useLocalStorage(key, initialValue);
+  return [values, setValues];
+};
+
+export default useDarkMode;
